703eecb6b7ab2836dfe9d4c0c3ac2b78ea02e727
Markus Strickert <markus.strickert@sigmatechnology.com>
2026-03-29 19:09:43 +0200

    Fix Firebase database region: point to europe-west1
    - Added databaseURL to firebaseConfig
    - Points to https://frick-budget-default-rtdb.europe-west1.firebasedatabase.app
    - Fixes warning: 'Database lives in a different region'

--- budget/firebase-sync.js
+++ budget/firebase-sync.js
@@ -22,7 +22,8 @@ const firebaseConfig = {
   projectId: "frick-budget",
   storageBucket: "frick-budget.firebasestorage.app",
   messagingSenderId: "231130144804",
-  appId: "1:231130144804:web:49ad446a858c585d2838b1"
+  appId: "1:231130144804:web:49ad446a858c585d2838b1",
+  databaseURL: "https://frick-budget-default-rtdb.europe-west1.firebasedatabase.app"
 };
 
 // Keys to sync (read from localStorage, write to Firebase)
