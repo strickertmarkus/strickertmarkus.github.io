6726eabe8bc17e6e02d0936a65b8dfcecbe62127
strickertmarkus <markus.strickert@sigmatechnology.com>
2026-08-28 13:39:59 +0200

    Remove superseded Exercise mobile toolbar fix

--- budget/exercise-mobile-week-toolbar-fix.js
+++
@@ -1,5 +0,0 @@
-(function () {
-  'use strict';
-  if (!/\/exercise\.html$/i.test(window.location.pathname)) return;
-  /* Compatibility shim. Mobile week-toolbar layout now lives in exercise-shell-v12.js. */
-})();
