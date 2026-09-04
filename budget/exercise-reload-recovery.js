1218df99ec0395301403e38d825a79830ea266ee
strickertmarkus <markus.strickert@sigmatechnology.com>
2026-09-04 14:33:35 +0200

    Load atomic custom transition fix

--- budget/exercise-reload-recovery.js
+++ budget/exercise-reload-recovery.js
@@ -63,6 +63,7 @@
 
   function loadSessionStability() {
     loadScriptOnce('exercise-session-stability-v55.js?v=20260904-1418-session-stable','data-exercise-session-stability-v55');
+    loadScriptOnce('exercise-custom-transition-atomic-v56.js?v=20260904-1432-atomic-custom','data-exercise-custom-transition-atomic-v56');
   }
 
   function loadLogLayers() {
